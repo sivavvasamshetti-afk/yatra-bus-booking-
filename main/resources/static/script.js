75d3734f3604178f23016bab03769fc516a26474
sivavvasamshetti-afk <siva.vvasamshetti@gmail.com>
2026-03-06 15:22:55 +0530

    Create script.js

---
+++ main/resources/static/script.js
@@ -0,0 +1,11 @@
+function searchBus(){
+
+let from=document.getElementById("from").value
+let to=document.getElementById("to").value
+let date=document.getElementById("date").value
+
+document.getElementById("result").innerHTML=
+
+"Searching buses from "+from+" to "+to+" on "+date
+
+}

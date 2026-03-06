171fc29ea8ec9301caaa14eff17d6862c2f74e14
sivavvasamshetti-afk <siva.vvasamshetti@gmail.com>
2026-03-06 15:14:54 +0530

    Create YatraApplication.java

---
+++ src/main/java/yatra/YatraApplication.java
@@ -0,0 +1,15 @@
+package com.yatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatraApplication {
+
+public static void main(String[] args) {
+
+SpringApplication.run(YatraApplication.class, args);
+
+}
+
+}
